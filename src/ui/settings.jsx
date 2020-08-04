5b628469259867a3bf6c830e845cf5b06e723563
EthanRutherford <ethanrutherford2@gmail.com>
2020-08-04 03:29:50 +0000

    Deploying to gh-pages from  @ 1867c7e01771ab21d4a33c4a815277a66416c031 🚀

--- src/ui/settings.jsx
+++ src/ui/settings.jsx
@@ -13,6 +13,7 @@ export function SettingsPopup({close}) {
 	const initialSettings = useMemo(loadSettings, []);
 	const [pickerMode, setPickerMode] = useState("hsv");
 	const [bgColor, setColor] = useColor(initialSettings.bgColor);
+	const [zoomScale, setZoomScale] = useState(initialSettings.zoomScale);
 	const [panScale, setPanScale] = useState(initialSettings.panScale);
 
 	const overlayClick = (event) => {
@@ -23,18 +24,32 @@ export function SettingsPopup({close}) {
 	const setBgColor = setColor.useWrapper((color) => {
 		saveSettings({bgColor: color.rgb, panScale});
 	});
+	const onZoomChange = (event) => {
+		setZoomScale(event.target.value);
+		const number = Number.parseFloat(event.target.value);
+		if (!Number.isNaN(number)) {
+			saveSettings({bgColor: bgColor.rgb, zoomScale: number, panScale});
+		}
+	};
+	const onZoomBlur = (event) => {
+		const number = Number.parseFloat(event.target.value);
+		if (Number.isNaN(number)) {
+			setZoomScale(1);
+			saveSettings({bgColor: bgColor.rgb, zoomScale: 1, panScale});
+		}
+	};
 	const onPanChange = (event) => {
 		setPanScale(event.target.value);
 		const number = Number.parseFloat(event.target.value);
 		if (!Number.isNaN(number)) {
-			saveSettings({bgColor: bgColor.rgb, panScale: number});
+			saveSettings({bgColor: bgColor.rgb, zoomScale, panScale: number});
 		}
 	};
 	const onPanBlur = (event) => {
 		const number = Number.parseFloat(event.target.value);
 		if (Number.isNaN(number)) {
 			setPanScale(1);
-			saveSettings({bgColor: bgColor.rgb, panScale: 1});
+			saveSettings({bgColor: bgColor.rgb, zoomScale, panScale: 1});
 		}
 	};
 
@@ -75,18 +90,32 @@ export function SettingsPopup({close}) {
 				) : (
 					<div>this shouldn't happen :(</div>
 				)}
-				<h2 className={styles.subHeader}>Pan/Zoom scale</h2>
+				<h2 className={styles.subHeader}>Zoom/Pan speed</h2>
 				<div className={styles.description}>
 					Adjust zoom and pan speed when using mousewheel or trackpad
 				</div>
-				<input
-					type="number"
-					className={styles.scaleInput}
-					step={.1}
-					value={panScale}
-					onChange={onPanChange}
-					onBlur={onPanBlur}
-				/>
+				<label className={styles.label}>
+					Zoom speed
+					<input
+						type="number"
+						className={styles.scaleInput}
+						step={.1}
+						value={zoomScale}
+						onChange={onZoomChange}
+						onBlur={onZoomBlur}
+					/>
+				</label>
+				<label className={styles.label}>
+					Pan speed
+					<input
+						type="number"
+						className={styles.scaleInput}
+						step={.1}
+						value={panScale}
+						onChange={onPanChange}
+						onBlur={onPanBlur}
+					/>
+				</label>
 				<button className={styles.done} onClick={close}>Done</button>
 			</div>
 		</div>
